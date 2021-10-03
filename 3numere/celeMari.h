a681df463a954edd8dfec96ddb211f89a05733a1
Cornea Adrian Emilian <emilian.cornea@yahoo.com>
2021-10-03 17:44:50 +0300

    comments placement

--- 3numere/celeMari.h
+++ 3numere/celeMari.h
@@ -1,4 +1,4 @@
 #pragma once
-//Functie ce printeaza cele mai mici/mari 3 numere dintr-un array
 #include <iostream>
+//Functie ce printeaza cele mai mici/mari 3 numere dintr-un array
 void celeMaiMari(int arr[],int dimensiune);
